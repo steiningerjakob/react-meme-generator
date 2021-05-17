081a0f87e6e4f360b853f3635ec5e23c4ff983bc
“Jakob <steiningerjakob@gmail.com>
2021-05-17 17:12:40 +0200

    Add search history and single button control

--- src/App.js
+++ src/App.js
@@ -4,30 +4,71 @@ import { css } from '@emotion/react';
 import { useState } from 'react';
 import UserInputs from './UserInputs.js';
 
+// Style element via CSS-in-JS
+
 const imageStyle = css`
   max-width: 300px;
   max-height: auto;
   margin: 20px;
 `;
+const pStyles = css`
+  font-size: 1.5em;
+  font-weight: bold;
+  margin: 5px;
+  text-align: center;
+`;
+const historyStyles = css`
+  background-color: #fafafa;
+  max-width: 400px;
+  padding: 5px;
+  margin: auto;
+  margin-top: 20px;
+  display: flexbox;
+  justify-content: center;
+  border: 1px solid #dcdcdc;
+  box-shadow: 1px 1px 8px 1px #dcdcdc;
+  border-radius: 5px;
+  text-align: left;
+  font-size: 12px;
+`;
 
 function App() {
-  const [topTextInput, setTopTextInput] = useState(['Launched 1000&1 memes']);
-  const [topTextOutput, setTopTextOutput] = useState(['Launched 1000&1 memes']);
-  const [bottomTextInput, setBottomTextInput] = useState([
+  // set state variables
+
+  const [topTextInput, setTopTextInput] = useState('Launched 1000&1 memes');
+  const [topTextOutput, setTopTextOutput] = useState('Launched 1000&1 memes');
+  const [bottomTextInput, setBottomTextInput] = useState(
     '#hated every one of them',
-  ]);
-  const [bottomTextOutput, setBottomTextOutput] = useState([
+  );
+  const [bottomTextOutput, setBottomTextOutput] = useState(
     '#hated every one of them',
-  ]);
-  const [imageInput, setImageInput] = useState(['grumpycat']);
-  const [imageOutput, setImageOutput] = useState(['grumpycat']);
+  );
+  const [imageInput, setImageInput] = useState('grumpycat');
+  const [imageOutput, setImageOutput] = useState('grumpycat');
+  const [memeHistory, setMemeHistory] = useState([]);
+  let historyId = 0;
+
+  // assemble URL string based on user inputs
+
   const stringInput = `https://api.memegen.link/images/${imageOutput}/${topTextOutput}/${bottomTextOutput}.png`;
   const stringOutput = stringInput
     .replace(/ /g, '_')
     .replace(/[#]/g, '~h')
     .replace(/&/g, '~a');
 
-  // const topTextHistory = [null];
+  // retrieve user input via button click and create history
+
+  function submitInput() {
+    setTopTextOutput(topTextInput);
+    setBottomTextOutput(bottomTextInput);
+    setImageOutput(imageInput);
+    const newMemeHistory = memeHistory.concat({
+      id: historyId,
+      name: `Top text: ${topTextInput}, bottom text: ${bottomTextInput}, image: ${imageInput}`,
+    });
+    historyId = historyId + 1;
+    setMemeHistory(newMemeHistory);
+  }
 
   // download function source:
   // https://stackoverflow.com/questions/49668363/html5-download-attribute-not-working-when-download-external-pdf-file-on-chrome/60299071#60299071
@@ -88,7 +129,9 @@ function App() {
         setBottomTextOutput={setBottomTextOutput}
         imageInput={imageInput}
         setImageInput={setImageInput}
+        imageOutput={imageOutput}
         setImageOutput={setImageOutput}
+        submitInput={submitInput}
       />
       <br />
       {/* Dispyay meme */}
@@ -97,22 +140,16 @@ function App() {
       <button onClick={() => downloadResource(stringOutput, 'MyMeme.jpeg')}>
         Download meme
       </button>
-      {/* WORK IN PROGRESS: button to add search terms to history */}
-      {/* <br />
-      <button
-        onClick={() => {
-          topTextHistory.push(topTextOutput);
-        }}
-      >
-        Add to history
-      </button>
-      <div>
+      <br />
+      {/* Display search history */}
+      <div css={historyStyles}>
+        <p css={pStyles}>Your meme history</p>
         <ul>
-          {topTextHistory.map((item) => {
-            return <li key={topTextHistory[item]}>{item}</li>;
+          {memeHistory.map((item) => {
+            return <li key={`Meme-History-Item-${item.id}`}>{item.name}</li>;
           })}
         </ul>
-      </div> */}
+      </div>
     </div>
   );
 }
--- src/UserInputs.js
+++ src/UserInputs.js
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useEffect } from 'react';
 
 // Style element via CSS-in-JS
-const sectionStyles = css`
+const divStyles = css`
   display: flex;
   justify-content: center;
 `;
@@ -19,46 +18,20 @@ const inputStyles = css`
   text-align: center;
 `;
 
-// Dropdown for image options - to be refined with entire list
-// via json data fetch
-const dropdownOptions = ['grumpycat', 'doge', 'bender', 'ams', 'cb', 'trump'];
-
 // Capture user input on top & bottom text and image, and change variable state accordingly
 // Pass state to other components via destructured props
 function UserInputs({
   topTextInput,
   setTopTextInput,
-  setTopTextOutput,
   bottomTextInput,
   setBottomTextInput,
-  setBottomTextOutput,
   imageInput,
   setImageInput,
-  setImageOutput,
+  submitInput,
 }) {
-  // function to delay processing of user input --> Question: variables are read in App.js files, is that ok?
-  useEffect(() => {
-    const timeOutTopText = setTimeout(
-      () => setTopTextOutput(topTextInput),
-      1000,
-    );
-    return () => clearTimeout(timeOutTopText);
-  }, [setTopTextOutput, topTextInput]);
-  useEffect(() => {
-    const timeOutBottomText = setTimeout(
-      () => setBottomTextOutput(bottomTextInput),
-      1000,
-    );
-    return () => clearTimeout(timeOutBottomText);
-  }, [setBottomTextOutput, bottomTextInput]);
-  useEffect(() => {
-    const timeOutImage = setTimeout(() => setImageOutput(imageInput), 1000);
-    return () => clearTimeout(timeOutImage);
-  }, [imageInput, setImageOutput]);
-
   return (
-    <div>
-      <section css={sectionStyles}>
+    <>
+      <div css={divStyles}>
         <label htmlFor="top">
           Top text:
           <br />
@@ -71,6 +44,7 @@ function UserInputs({
             }}
             css={inputStyles}
           />
+          <br />
         </label>
 
         <label htmlFor="bottom">
@@ -90,7 +64,7 @@ function UserInputs({
         <label htmlFor="image">
           Image:
           <br />
-          <select
+          <input
             id="image"
             placeholder="grumpycat"
             value={imageInput}
@@ -98,14 +72,14 @@ function UserInputs({
               setImageInput(event.currentTarget.value);
             }}
             css={inputStyles}
-          >
-            {dropdownOptions.map((item) => {
-              return <option key={item}>{item}</option>;
-            })}
-          </select>
+          />
         </label>
-      </section>
-    </div>
+        <br />
+      </div>
+      <div>
+        <button onClick={submitInput}>Create meme</button>
+      </div>
+    </>
   );
 }
 
